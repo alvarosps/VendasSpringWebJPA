565360db69a0326353a799ea671c853d03f83dc7
Erick Melo de Moura <emoura@cipucrs.pucrs.br>
2017-08-25 11:21:58 -0300

    Adding conf files to mourinha branch

---
+++ src/main/java/com/example/secure/WebSecurityConfigVendas.java
@@ -0,0 +1,36 @@
+package com.example.secure;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfigVendas extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception{
+		
+		http
+        .authorizeRequests()
+            .antMatchers("/", "/home").permitAll()
+            .anyRequest().authenticated()
+            .and()
+        .formLogin()
+            .loginPage("/login")
+            .permitAll()
+            .and()
+        .logout()
+            .permitAll();
+	}
+	
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception{
+		auth
+			.inMemoryAuthentication()
+				.withUser("usuario").password("senha").roles("USER");
+	}
+}
