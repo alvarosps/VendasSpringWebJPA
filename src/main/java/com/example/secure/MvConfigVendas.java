920a250953ca24ed69baacfc4fb255c920bad038
Erick Melo de Moura <emoura@cipucrs.pucrs.br>
2017-08-25 10:50:12 -0300

    Inserting WebSecure files to repository

---
+++ src/main/java/com/example/secure/MvConfigVendas.java
@@ -0,0 +1,19 @@
+package com.example.secure;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class MvConfigVendas extends WebMvcConfigurerAdapter{
+
+	@Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/logado").setViewName("logado");
+        registry.addViewController("/login").setViewName("login");
+    }
+	
+}
