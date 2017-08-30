81c44e4ab58a4da3cc06a0072b5d71e7ec598ed9
Erick Melo de Moura <emoura@cipucrs.pucrs.br>
2017-08-30 11:09:12 -0300

    Reformulando a classe Venda

--- src/main/java/com/example/demo/Venda.java
+++ src/main/java/com/example/demo/Venda.java
@@ -10,40 +10,43 @@ import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
+//criando tabela Venda
 @Entity
-@Table(name="Vendas")
-public class Venda {
-	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	private int id;
-	//@Column(columnDefinition="DATE")
-	//private LocalDate data;
-	@OneToMany(fetch=FetchType.EAGER, cascade= {CascadeType.PERSIST,CascadeType.MERGE})
-	private List<ItemDeVenda> itensDeVenda;
+@Table(name="vendas")
+public class Venda{
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="seq_venda")
+	@SequenceGenerator(name="seq_venda", sequenceName="seq_venda", initialValue = 1, allocationSize = 1)
+	private int id;
+	
+	@OneToMany(fetch=FetchType.EAGER, cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+	@JoinColumn(name = "venda_id")
+	private List<ItemDeVenda> itensDeVenda;
+
+	//gets e sets
 	public int getId() {
 		return id;
 	}
-	
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
 	public List<ItemDeVenda> getItensDeVenda() {
 		return itensDeVenda;
 	}
-	public void vender(Produto prod, int quant) {
-		ItemDeVenda item = new ItemDeVenda();
-		item.setProduto(prod);
-		item.setQuantidade(quant);
-		itensDeVenda.add(item);
+
+	public void setItensDeVenda(List<ItemDeVenda> itensDeVenda) {
+		this.itensDeVenda = itensDeVenda;
 	}
-	public double getTotal() {
-		double soma = 0.0;
-		for(ItemDeVenda item : itensDeVenda) {
-			soma += item.getSubTotal();
-		}
-		return soma;
-	}
+	
+	
 }
